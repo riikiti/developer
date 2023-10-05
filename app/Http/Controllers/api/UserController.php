1ac4e34087afb81ce542ac63dabb8e2971dfc3f3
Ruslan <rik.chanel@gmail.com>
2023-10-05 14:31:30 +0300

    1 complete 80%

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+        return User::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserStoreRequest $request, User $user)
+    {
+        $user->update($request->validated());
+        return new  UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(null);
+    }
+}
---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email'=>'string|email|max:255|required',
+            'username'=>'required|string|max:255|regex:/^[A-Za-z0-9]+$/i|unique:users',
+            'name'=>'string|nullable|min:2|max:255',
+            'password' => 'string|min:6',
+        ];
+    }
+}
